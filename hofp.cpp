bae1976a62c67b7eb67565460dc786f4d9548ec5
laura-mm <laura.mm@hotmail.co.uk>
2020-03-09 00:35:54 +0000

    can find divergence limit and better solution for M for p = 2,3

--- hofp.cpp
+++ hofp.cpp
@@ -66,7 +66,9 @@ double Mgrad(double z1, double MM, double small)
 {
 	double Mcoeff = help(z1)*z1/first(z1); // may be better because doesnt rely on other parameters
 	double grad = (Mcoeff*(MM + small)) + (mu*pow((MM + small), p - 1.0)) + k;
+	cout << "grad1 " << grad << endl;
 	grad -= (Mcoeff*(MM - small)) + (mu*pow((MM - small), p - 1.0)) + k;
+	cout << "grad2 " << grad << endl;
 	return grad/(2.0*small);
 }
 
@@ -82,61 +84,58 @@ double M(double Mstart, double z1) //this is a solution to a p-order polynomial,
 
 	cout << "now plotting function for M for z1 = " << z1 << endl;
 	
-	for (int i = 0; i <= 200000; i++)
+	for (int i = 0; i <= 400000; i++)
 	{
-		double m = (0.00001*(double)i);
+		double m = (0.1*(double)i);
 		mm << m;
 		ab << (Mcoeff*m) + (mu*pow(m, p - 1.0)) + k;
-		if (i != 200000) {mm << ", "; ab << ", ";}
+		if (i != 400000) {mm << ", "; ab << ", ";}
 	}
 	mm.close(); ab.close();
 	cout << "finished plotting M" << endl;
+	}
+	
+	if (p == 2.0) return -k/((z1*help(z1)/first(z1)) + mu); // this could give -infinity
+	if (p == 3.0)
+	{
+		if (mu == 0.0) return -k*first(z1)/(z1*help(z1));
+		else
+		{
+			if (pow(z1*help(z1)/first(z1), 2.0) < 4.0*mu*k) return 1.0/0.0;
+			else return ((z1*help(z1)/first(z1)) + sqrt(pow(z1*help(z1)/first(z1), 2.0) - (4.0*mu*k)))/(-2.0*mu);
+		}
 	}
 	
 
 	double small = pow(10.0, -6.0); // dont know about this?
 	double y = (Mcoeff*MM) + (mu*pow(MM, p - 1.0)) + k;
 	double grad;
-	//if (info == true) {cout << "z1 " << z1 << ", y " << y << endl;}
+	//if (info == true) {cout << "MM " << MM << ", y " << y << endl;}
 	double ynew;
 	double Mnew;
 	while (abs(y) > 0.0)
 	{
+		if (info == true) {cout << "MM " << MM << ", y " << y << endl;}
 		grad = Mgrad(z1, MM, small);
-		if (grad == 0.0) break; //{cout << "grad 0 " << g(z1, zz + small) << ", " << g(z1, zz - small) << endl;}
+		if (grad == 0.0) {cout << "grad = 0" << endl; if (abs(y) < pow(10.0, -5.0)) return MM; else return 1.0/0.0;}
 		//if (info == true) {cout << "small " << small << " grad " << grad << endl;}
 		Mnew = MM - (y/grad);
 		// if (z1 + zz < 0.0) cout << "badd" << endl;
 		ynew = (Mcoeff*Mnew) + (mu*pow(Mnew, p - 1.0)) + k;
-		if (abs(ynew) >= abs(y) && ((ynew*y) > 0.0 || abs(y) < pow(10.0, -5.0))) break;
+		if (abs(ynew) >= abs(y) && abs(y) < pow(10.0, -5.0)) break;
+		if (abs(ynew) >= abs(y) && y*ynew > 0.0) return 1.0/0.0;
 		y = ynew;
 		MM = Mnew;
 		//if (info == true) {cout << "z1 " << z1 << ", y " << y << endl;}
-		if (small > pow(10.0, -10.0)) small /= 10.0;
+		if (small > pow(10.0, -9.0)) small /= 10.0;
+		cout << "small " << small << endl;
 	}
 	
 	//if (!(abs(sigma(z1, z2)) >= 0.0  && abs(gamma(z1, z2)) >= 0.0)) // found the wrong solution
 
 	return MM;
 }
-/*
-double melp(double z1) // (k + mutot), maybe like help but for M
-{
-	double Mstart = k;
-	double melp = k + (mu*pow(M(Mstart, z1), p - 1.0));
-	if (melp == 0.0) cout << "melp = 0!! :(" << endl;
-	return melp;
-}
-// this could be zero for a particular mu, but thats another problem
 
-double sigtot(double z1)
-{
-	//if (z1 == 1.0/0.0) return 0.0;
-	if (z1 == 0.0) cout << "z1 = 0 in sigtot function!! :(" << endl; // I dont know about this!
-	return -melp(z1)/z1;
-}
-// this can be zero if melp is zero, but this can be undefined if z1 = 0, and thats probably a worse problem
-*/
 
 double sigtot(double z1)
 {
@@ -154,18 +153,21 @@ double q(double z1)
 double sigma(double z1)
 {
 	//if (z1 == 1.0/0.0) return 0;
-	return sqrt(2.0/(p*pow(q(z1), p - 1.0)))*sigtot(z1);
+	//return sqrt(2.0/(p*pow(q(z1), p - 1.0)))*sigtot(z1);
 	//return sqrt(2.0/(second(z1)*p*pow(q(z1), p - 2.0)))*help(z1);
+	
+	
+	return help(z1)*sqrt(2.0/(p*pow(second(z1), p - 1.0)))*pow(first(z1)/M(k, z1), p - 2.0); // this one!
+	//return help(z1)*sqrt(2.0/(p*pow(second(z1), p - 1.0)));
+}
+
+double mud2(double z1) // for p = 2 this is the value of mu where we find divergence
+{
+	return -z1*help(z1)/first(z1);
 }
 
 ///////////////////////////////// end of equation type stuff
 
-double gradient(double z1, double small)
-{
-	double grad = second(z1 + small) - (phi(z1 + small)*(p - 1.0));
-	grad -= (second(z1 - small) - (phi(z1 - small)*(p - 1.0)));
-	return grad/(2.0*small);
-}
 
 double crit_z1() // depends on p only
 {	
@@ -173,50 +175,76 @@ double crit_z1() // depends on p only
 	double small = pow(10.0, -6.0);
 	double y = second(z1) - (phi(z1)*(p - 1.0));
 	double grad;
-	if (info == true) {cout << "z1 " << z1 << ", y " << y << endl;}
+	//if (info == true) {cout << "z1 " << z1 << ", y " << y << endl;}
 	double ynew;
 	double znew;
 	while (abs(y) > 0.0)
 	{
-		grad = gradient(z1, small);
+		grad = ((second(z1 + small) - (phi(z1 + small)*(p - 1.0))) - (second(z1 - small) - (phi(z1 - small)*(p - 1.0))))/(2.0*small);
 		if (grad == 0.0) break;
-		if (info == true) {cout << "small " << small << " grad " << grad << endl;}
+		//if (info == true) {cout << "small " << small << " grad " << grad << endl;}
 		znew = z1 - (y/grad);
 		ynew = second(znew) - (phi(znew)*(p - 1.0));
 		if (abs(ynew) >= abs(y) && ((ynew*y) > 0.0 || abs(y) < pow(10.0, -5.0))) break;
 		y = ynew;
 		z1 = znew;
-		if (info == true) {cout << "z1 " << z1 << ", y " << y << endl;}
+		//if (info == true) {cout << "z1 " << z1 << ", y " << y << endl;}
 		if (small > pow(10.0, -10.0)) small /= 10.0;
 	}
 	
-	/*if (!(abs(sigma(z1, z2)) >= 0.0  && abs(gamma(z1, z2)) >= 0.0)) // found the wrong solution
-	{
-		small = pow(10.0, -6.0);
-		grad = gradient(z1, z2, small);
-		//z1 -= 2.0*y/grad;
-		
-		z1 *= -1.0;
-		y = (pow(param3(z1, z2), 2.0)*q(z1, z2)) - phi(z1, z2);
-		if (info == true) {cout << "z1 " << z1 << ", y " << y << endl;}
-		
-		while (abs(y) > 0.0) // assume there are only 2 roots
-		{
-			grad = gradient(z1, z2, small);
-			if (grad == 0.0) break; //{cout << "grad 0 " << g(z1, zz + small) << ", " << g(z1, zz - small) << endl;}
-			if (info == true) {cout << "small " << small << " grad " << grad << endl;}
-			znew = z1 - (y/grad);
-			// if (z1 + zz < 0.0) cout << "badd" << endl;
-			ynew = (pow(param3(znew, z2), 2.0)*q(znew, z2)) - phi(znew, z2);
-			if (abs(ynew) >= abs(y) && ((ynew*y) > 0.0 || abs(y) < pow(10.0, -5.0))) break;
-			y = ynew;
-			z1 = znew;
-			if (info == true) {cout << "z1 " << z1 << ", y " << y << endl;}
-			if (small > pow(10.0, -10.0)) small /= 10.0;
-		}
-	}*/
-	
 	return z1;	
+}
+
+
+double div2_z1() // for p = 2, depends on mu
+{
+	if (info == true)
+	{
+	ofstream zz; zz.open("zz.txt");
+	ofstream ab; ab.open("abs.txt");
+
+	cout << "now plotting function for zdiv for mu = " << mu << endl;
+	
+	for (int i = 0; i <= 400000; i++)
+	{
+		double z = (10.0*(double)i/400000.0) - 5.0;
+		zz << z;
+		ab << z*help(z)/first(z) + mu;
+		if (i != 400000) {zz << ", "; ab << ", ";}
+	}
+	zz.close(); ab.close();
+	cout << "finished plotting z" << endl;
+	}
+	
+	if (mu == 1.0) return -1.0/0.0;
+	
+	double z1 = 2.0;
+	double small = pow(10.0, -6.0);
+	double y = z1*help(z1)/first(z1) + mu;
+	double grad;
+	double ynew;
+	double znew;
+	while (abs(y) > 0.0)
+	{
+		if (info == true) {cout << "z1 " << z1 << ", y " << y << endl;}
+		grad = (((z1 + small)*help(z1 + small)/first(z1 + small)) - ((z1 - small)*help(z1 - small)/first(z1 - small)))/(2.0*small);
+		//cout << "grad " << grad << endl;
+		//cout << "grad1 " << ((z1 + small)*help(z1 + small)/first(z1 + small)) << endl;
+		//cout << "z1 + small " << z1 + small << endl;
+		//cout << "help(z1 + small) " << help(z1 + small) << endl;
+		//cout << "first(z1 + small) " << first(z1 + small) << endl;
+		if (grad == 0) {if (abs(y) < pow(10.0, -5.0)) return z1; else return -1.0/0.0;}
+		znew = z1 - (y/grad);
+		ynew = znew*help(znew)/first(znew) + mu;
+		if (info == true) {cout << "znew " << znew << ", ynew " << ynew << endl;}
+		//cout << "abs(ynew) " << abs(ynew) << ", abs(y) " << abs(y) << endl;
+		if (abs(ynew) >= abs(y) && ((ynew*y) > 0.0 || abs(y) < pow(10.0, -5.0))) break;
+		y = ynew;
+		z1 = znew;
+		if (small > pow(10.0, -10.0)) small /= 10.0;
+	}
+	
+	return z1;
 }
 	
 
@@ -260,7 +288,7 @@ void fiveplot(int grid)
 			if (sigma(z1) < 0.0) continue; //{cout << "bad solution 3 " << z1 << endl; continue;}
 			//if (!(abs(sigma(z1)) >= 0.0)) continue; //{cout << "bad solution 4 " << z1 << endl; continue;}
 			if (sigma(z1) > 10.0) continue;
-			if (sigma(z1) < sig_prev) break;
+			//if (sigma(z1) < sig_prev) break; // this one!
 			//if (sig(z1, z2) == 0.0) continue; // this is for histograms only
 
 			ArrayXd entry = ArrayXd::Zero(6); // sigma, z1, phi, M, q, help
@@ -271,11 +299,13 @@ void fiveplot(int grid)
 			entry(4) = q(z1);
 			entry(5) = help(z1);
 			
+			if (gama == 0.0) cout << sigma(z1) << ", " << pow(2.0/3.0, 0.5)*z1*-1.0/second(z1) << ", " << M(Mstart, z1) << ", " << -1.0*first(z1)/z1 << endl;
+			
 			Mstart = entry(3);
 			sig_prev = entry(0);
 			
 			sigmat.push_back(entry);
-			cout << "i " << i << ", z1 " << z1 << ", phi " << phi(z1) << ", M " << entry(3) << ", sig " << sigma(z1) << ", q " << q(z1) << endl;
+			//cout << "i " << i << ", z1 " << z1 << ", phi " << phi(z1) << ", M " << entry(3) << ", sig " << sigma(z1) << ", q " << q(z1) << endl;
 			//cin >> carryon;
 			//info = true;
 		}
@@ -295,7 +325,7 @@ void fiveplot(int grid)
 	critfile.close();
 }
 
-void phase(int grid)
+void phase(int grid) // for gamma against sigma
 {
 	string filename = "phline_" + to_string((int)p) + "_" + to_string((int)(10*mu)) + ".txt";
 	ofstream file; file.open(filename);
@@ -307,6 +337,35 @@ void phase(int grid)
 		file << gama << "," << sigma(zcrit);
 		if (ig != grid) file << ",";
 	}
+	file.close();
+}
+
+void bunin(int grid) // for a phase diagram like bunin
+{
+	ofstream file; file.open("bunin2_" + to_string((int)(10.0*gama)) + ".txt");
+	
+	for (int i = 0; i <= grid; i++)
+	{
+		double z1 = 100.0*(double)i/(double)grid - 50.0;
+		if (second(z1)/(phi(z1)*(p - 1.0)) <= -gama) break;
+		mu = mud2(z1);
+		if (mu < -10.0) break;
+		//cout << z1 << ", " << sigma(z1) << ", " << mu << endl;
+		if (i != 0) file << ",";
+		file << mu << "," << sigma(crit_z1()) << "," << sigma(z1);
+		cout << z1 << ", " << mu << "," << sigma(crit_z1()) << "," << sigma(z1) << endl;
+	}
+	
+	
+	/*for (int i = 0; i <= grid; i ++)
+	{
+		mu = (6.0*double(i)/(double)grid) - 5.0;
+		file << mu << "," << sigma(crit_z1()) << "," << sigma(div2_z1());
+		cout << mu << "," << sigma(crit_z1()) << "," << sigma(div2_z1()) << endl;
+		//if (z1/second(z1) > prev) cout << z1 << ", " << prev << endl << endl;
+		//prev = z1/second(z1);
+		if (i != grid) file << ",";
+	}*/
 	file.close();
 }
 
@@ -377,17 +436,70 @@ void fiveplot(int grid)
 }
 */
 
+void testing(int grids)
+{
+	ofstream file; file.open("testing.txt");
+	cout << crit_z1() << endl;
+	double prev = 1.0;
+	for (int i = 0; i <= grids; i ++)
+	{
+		double z1 = (5.0*double(i)/(double)grids) - 4.0;
+		file << z1 << "," << first(z1) << "," << mu - (z1*help(z1)/first(z1));
+		cout << z1 << ", " << -z1/first(z1) << endl;
+		//if (z1/second(z1) > prev) cout << z1 << ", " << prev << endl << endl;
+		//prev = z1/second(z1);
+		if (i != grids) file << ",";
+	}
+	file.close();
+}
+		
+
 int main()
 {
 	int grids = 400000; //400000; // for graphs with varying sigma
 	p = 2.0;
-	//gama = -0.1;
-	mu = -2.0;
+	gama = 1.0;
+	mu = 0.0;
 	k = 1.0;
 	
 	//fiveplot(grids);
 	
-	phase(grids);
+	//phase(grids);
+	
+	//testing(grids);
+	
+	//info = true;
+	
+	//cout << crit_z1() << ", " << sigma(crit_z1()) << endl;
+	
+	//cout << div2_z1() << ", " << sigma(div2_z1()) << endl;
+	
+	//cout << div2_z1() << endl;
+	
+	
+	bunin(100000);
+	
+	
+	
+	//double z1 = -100.0;
+	
+	//cout << help(z1) << endl;
+	
+	//cout << z1 << endl;
+	
+	//cout << help(z1) << endl;
+	
+	//cout << M(1.0, z1) << endl;
+	
+	//cout << (-z1*help(z1)/first(z1))/(2.0*mu) << endl;
+	
+	//cout << (sqrt(pow(z1*help(z1)/first(z1), 2.0) - (4.0*mu*k)))/(2.0*mu)
+	
+	//cout << (sqrt(pow(z1*help(z1)/first(z1), 2.0) - (4.0*mu*k)) - (z1*help(z1)/first(z1)))/(2.0*mu) << endl;
+	
+	//cout << sigma(z1) << endl;
+	
+
 	
 	info = false;
 	
