c2f14479818b432f501d9abde7e292eafa02ecd4
laura-mm <laura.mm@hotmail.co.uk>
2020-03-13 21:20:53 +0000

    for testing

---
+++ testing.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+z = np.loadtxt('testing.txt', delimiter = ",")
+z = z.reshape(len(z)/3, 3)
+z = np.transpose(z)
+
+plt.figure(1)
+plt.plot(z[0], z[1])
+plt.plot(z[0], z[2])
+plt.axhline(y = 0, linestyle = '--', color = 'r')
+plt.ylim([-10, 10])
+plt.show()
